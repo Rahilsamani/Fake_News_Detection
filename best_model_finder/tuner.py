780affe973eb313928dc03504867b4fd024409ed
Rahilsamani <rahilahmed1720@gmail.com>
2024-10-06 18:17:54 +0530

    update tuner file

--- best_model_finder/tuner.py
+++ best_model_finder/tuner.py
@@ -1,170 +1,133 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-from xgboost import XGBClassifier
-from sklearn.metrics  import roc_auc_score,accuracy_score
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.model_selection import train_test_split
+from nature_inspired_algorithms import GWO, PSO, GA, ACO, Firefly # Placeholder for imported algorithms
 
-class Model_Finder:
+class Model_Finder_NatureInspired:
     """
-                This class shall  be used to find the model with best accuracy and AUC score.
-                Written By: iNeuron Intelligence
-                Version: 1.0
-                Revisions: None
+        This class is designed to find the best model using Nature-Inspired algorithms such as:
+        - Grey Wolf Optimizer (GWO) + Particle Swarm Optimization (PSO)
+        - Genetic Algorithm (GA)
+        - Ant Colony Optimization (ACO)
+        - Firefly Algorithm
+    """
 
-                """
-
-    def __init__(self,file_object,logger_object):
+    def __init__(self, file_object, logger_object):
         self.file_object = file_object
         self.logger_object = logger_object
-        self.clf = RandomForestClassifier()
-        self.xgb = XGBClassifier(objective='binary:logistic')
+        # Initialize nature-inspired algorithms
+        self.gwo = GWO()
+        self.pso = PSO()
+        self.ga = GA()
+        self.aco = ACO()
+        self.firefly = Firefly()
 
-    def get_best_params_for_random_forest(self,train_x,train_y):
+    def get_best_params_for_gwo_pso(self, train_x, train_y):
         """
-                                Method Name: get_best_params_for_random_forest
-                                Description: get the parameters for Random Forest Algorithm which give the best accuracy.
-                                             Use Hyper Parameter Tuning.
-                                Output: The model with the best parameters
-                                On Failure: Raise Exception
-
-                                Written By: iNeuron Intelligence
-                                Version: 1.0
-                                Revisions: None
-
-                        """
-        self.logger_object.log(self.file_object, 'Entered the get_best_params_for_random_forest method of the Model_Finder class')
+        Method to optimize parameters using Grey Wolf Optimizer (GWO) and Particle Swarm Optimization (PSO).
+        """
+        self.logger_object.log(self.file_object, 'Entered the get_best_params_for_gwo_pso method of the Model_Finder_NatureInspired class')
         try:
-            # initializing with different combination of parameters
-            self.param_grid = {"n_estimators": [10, 50, 100, 130], "criterion": ['gini', 'entropy'],
-                               "max_depth": range(2, 4, 1), "max_features": ['auto', 'log2']}
+            # Apply GWO to find optimal hyperparameters
+            gwo_best_params = self.gwo.optimize(train_x, train_y)
+            self.logger_object.log(self.file_object, 'GWO best params: ' + str(gwo_best_params))
 
-            #Creating an object of the Grid Search class
-            self.grid = GridSearchCV(estimator=self.clf, param_grid=self.param_grid, cv=5,  verbose=3)
-            #finding the best parameters
-            self.grid.fit(train_x, train_y)
+            # Use the GWO parameters to further optimize with PSO
+            pso_best_params = self.pso.optimize(train_x, train_y, gwo_best_params)
+            self.logger_object.log(self.file_object, 'PSO best params after GWO: ' + str(pso_best_params))
 
-            #extracting the best parameters
-            self.criterion = self.grid.best_params_['criterion']
-            self.max_depth = self.grid.best_params_['max_depth']
-            self.max_features = self.grid.best_params_['max_features']
-            self.n_estimators = self.grid.best_params_['n_estimators']
-
-            #creating a new model with the best parameters
-            self.clf = RandomForestClassifier(n_estimators=self.n_estimators, criterion=self.criterion,
-                                              max_depth=self.max_depth, max_features=self.max_features)
-            # training the mew model
-            self.clf.fit(train_x, train_y)
-            self.logger_object.log(self.file_object,
-                                   'Random Forest best params: '+str(self.grid.best_params_)+'. Exited the get_best_params_for_random_forest method of the Model_Finder class')
-
-            return self.clf
+            # Train the final model with optimized parameters (assuming the model to be optimized is something like SVM or Logistic Regression)
+            self.model = SomeMLModel(**pso_best_params)  # Placeholder for actual model
+            self.model.fit(train_x, train_y)
+            return self.model
         except Exception as e:
-            self.logger_object.log(self.file_object,
-                                   'Exception occured in get_best_params_for_random_forest method of the Model_Finder class. Exception message:  ' + str(
-                                       e))
-            self.logger_object.log(self.file_object,
-                                   'Random Forest Parameter tuning  failed. Exited the get_best_params_for_random_forest method of the Model_Finder class')
+            self.logger_object.log(self.file_object, f"Exception in get_best_params_for_gwo_pso: {str(e)}")
             raise Exception()
 
-    def get_best_params_for_xgboost(self,train_x,train_y):
-
+    def get_best_params_for_ga(self, train_x, train_y):
         """
-                                        Method Name: get_best_params_for_xgboost
-                                        Description: get the parameters for XGBoost Algorithm which give the best accuracy.
-                                                     Use Hyper Parameter Tuning.
-                                        Output: The model with the best parameters
-                                        On Failure: Raise Exception
-
-                                        Written By: iNeuron Intelligence
-                                        Version: 1.0
-                                        Revisions: None
-
-                                """
-        self.logger_object.log(self.file_object,
-                               'Entered the get_best_params_for_xgboost method of the Model_Finder class')
+        Method to optimize model parameters using Genetic Algorithm (GA).
+        """
+        self.logger_object.log(self.file_object, 'Entered the get_best_params_for_ga method of the Model_Finder_NatureInspired class')
         try:
-            # initializing with different combination of parameters
-            self.param_grid_xgboost = {
+            # Apply Genetic Algorithm to find the optimal parameters
+            ga_best_params = self.ga.optimize(train_x, train_y)
+            self.logger_object.log(self.file_object, 'GA best params: ' + str(ga_best_params))
 
-                'learning_rate': [0.5, 0.1, 0.01, 0.001],
-                'max_depth': [3, 5, 10, 20],
-                'n_estimators': [10, 50, 100, 200]
-
-            }
-            # Creating an object of the Grid Search class
-            self.grid= GridSearchCV(XGBClassifier(objective='binary:logistic'),self.param_grid_xgboost, verbose=3,cv=5)
-            # finding the best parameters
-            self.grid.fit(train_x, train_y)
-
-            # extracting the best parameters
-            self.learning_rate = self.grid.best_params_['learning_rate']
-            self.max_depth = self.grid.best_params_['max_depth']
-            self.n_estimators = self.grid.best_params_['n_estimators']
-
-            # creating a new model with the best parameters
-            self.xgb = XGBClassifier(learning_rate=self.learning_rate, max_depth=self.max_depth, n_estimators=self.n_estimators)
-            # training the mew model
-            self.xgb.fit(train_x, train_y)
-            self.logger_object.log(self.file_object,
-                                   'XGBoost best params: ' + str(
-                                       self.grid.best_params_) + '. Exited the get_best_params_for_xgboost method of the Model_Finder class')
-            return self.xgb
+            # Train the final model with optimized parameters
+            self.model = SomeMLModel(**ga_best_params)  # Placeholder for actual model
+            self.model.fit(train_x, train_y)
+            return self.model
         except Exception as e:
-            self.logger_object.log(self.file_object,
-                                   'Exception occured in get_best_params_for_xgboost method of the Model_Finder class. Exception message:  ' + str(
-                                       e))
-            self.logger_object.log(self.file_object,
-                                   'XGBoost Parameter tuning  failed. Exited the get_best_params_for_xgboost method of the Model_Finder class')
+            self.logger_object.log(self.file_object, f"Exception in get_best_params_for_ga: {str(e)}")
             raise Exception()
 
-
-    def get_best_model(self,train_x,train_y,test_x,test_y):
+    def get_best_params_for_aco(self, train_x, train_y):
         """
-                                                Method Name: get_best_model
-                                                Description: Find out the Model which has the best AUC score.
-                                                Output: The best model name and the model object
-                                                On Failure: Raise Exception
-
-                                                Written By: iNeuron Intelligence
-                                                Version: 1.0
-                                                Revisions: None
-
-                                        """
-        self.logger_object.log(self.file_object,
-                               'Entered the get_best_model method of the Model_Finder class')
-        # create best model for XGBoost
+        Method to optimize model parameters using Ant Colony Optimization (ACO).
+        """
+        self.logger_object.log(self.file_object, 'Entered the get_best_params_for_aco method of the Model_Finder_NatureInspired class')
         try:
-            self.xgboost= self.get_best_params_for_xgboost(train_x,train_y)
-            self.prediction_xgboost = self.xgboost.predict(test_x) # Predictions using the XGBoost Model
+            # Apply ACO to find the optimal parameters
+            aco_best_params = self.aco.optimize(train_x, train_y)
+            self.logger_object.log(self.file_object, 'ACO best params: ' + str(aco_best_params))
 
-            if len(test_y.unique()) == 1: #if there is only one label in y, then roc_auc_score returns error. We will use accuracy in that case
-                self.xgboost_score = accuracy_score(test_y, self.prediction_xgboost)
-                self.logger_object.log(self.file_object, 'Accuracy for XGBoost:' + str(self.xgboost_score))  # Log AUC
-            else:
-                self.xgboost_score = roc_auc_score(test_y, self.prediction_xgboost) # AUC for XGBoost
-                self.logger_object.log(self.file_object, 'AUC for XGBoost:' + str(self.xgboost_score)) # Log AUC
-
-            # create best model for Random Forest
-            self.random_forest=self.get_best_params_for_random_forest(train_x,train_y)
-            self.prediction_random_forest=self.random_forest.predict(test_x) # prediction using the Random Forest Algorithm
-
-            if len(test_y.unique()) == 1:#if there is only one label in y, then roc_auc_score returns error. We will use accuracy in that case
-                self.random_forest_score = accuracy_score(test_y,self.prediction_random_forest)
-                self.logger_object.log(self.file_object, 'Accuracy for RF:' + str(self.random_forest_score))
-            else:
-                self.random_forest_score = roc_auc_score(test_y, self.prediction_random_forest) # AUC for Random Forest
-                self.logger_object.log(self.file_object, 'AUC for RF:' + str(self.random_forest_score))
-
-            #comparing the two models
-            if(self.random_forest_score <  self.xgboost_score):
-                return 'XGBoost',self.xgboost
-            else:
-                return 'RandomForest',self.random_forest
-
+            # Train the final model with optimized parameters
+            self.model = SomeMLModel(**aco_best_params)  # Placeholder for actual model
+            self.model.fit(train_x, train_y)
+            return self.model
         except Exception as e:
-            self.logger_object.log(self.file_object,
-                                   'Exception occured in get_best_model method of the Model_Finder class. Exception message:  ' + str(
-                                       e))
-            self.logger_object.log(self.file_object,
-                                   'Model Selection Failed. Exited the get_best_model method of the Model_Finder class')
+            self.logger_object.log(self.file_object, f"Exception in get_best_params_for_aco: {str(e)}")
             raise Exception()
 
+    def get_best_params_for_firefly(self, train_x, train_y):
+        """
+        Method to optimize model parameters using Firefly Algorithm.
+        """
+        self.logger_object.log(self.file_object, 'Entered the get_best_params_for_firefly method of the Model_Finder_NatureInspired class')
+        try:
+            # Apply Firefly Algorithm to find the optimal parameters
+            firefly_best_params = self.firefly.optimize(train_x, train_y)
+            self.logger_object.log(self.file_object, 'Firefly best params: ' + str(firefly_best_params))
+
+            # Train the final model with optimized parameters
+            self.model = SomeMLModel(**firefly_best_params)  # Placeholder for actual model
+            self.model.fit(train_x, train_y)
+            return self.model
+        except Exception as e:
+            self.logger_object.log(self.file_object, f"Exception in get_best_params_for_firefly: {str(e)}")
+            raise Exception()
+
+    def get_best_model(self, train_x, train_y, test_x, test_y):
+        """
+        Find the best model by comparing AUC scores from different Nature-Inspired algorithms.
+        """
+        self.logger_object.log(self.file_object, 'Entered the get_best_model method of the Model_Finder_NatureInspired class')
+        try:
+            # Optimize models using different nature-inspired algorithms
+            model_gwo_pso = self.get_best_params_for_gwo_pso(train_x, train_y)
+            model_ga = self.get_best_params_for_ga(train_x, train_y)
+            model_aco = self.get_best_params_for_aco(train_x, train_y)
+            model_firefly = self.get_best_params_for_firefly(train_x, train_y)
+
+            # Evaluate models on the test set
+            models = [model_gwo_pso, model_ga, model_aco, model_firefly]
+            best_model = None
+            best_score = 0
+
+            for model in models:
+                predictions = model.predict(test_x)
+                if len(test_y.unique()) == 1:  # if there is only one label in y
+                    score = accuracy_score(test_y, predictions)
+                else:
+                    score = roc_auc_score(test_y, predictions)
+                
+                if score > best_score:
+                    best_score = score
+                    best_model = model
+
+            self.logger_object.log(self.file_object, f"Best model selected with score: {best_score}")
+            return best_model
+
+        except Exception as e:
+            self.logger_object.log(self.file_object, f"Exception in get_best_model: {str(e)}")
+            raise Exception()
